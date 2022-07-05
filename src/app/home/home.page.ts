7f82cc4b824d90ba5ce0f55d54c7137f60266afb
Thomas Alexaline <thomas.alexaline@wiilog.fr>
2022-07-05 16:48:40 +0200

    WIIS-7714 WIIS-7715: tagging is functional + added lib
    tagging is functional but the code still need some cleaning. Added the necessary lib and dependencies.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NFC, Ndef, NfcTag} from '@ionic-native/nfc/ngx';
+import {ViewWillEnter, ViewWillLeave} from '@ionic/angular';
+import {Observable, Observer, Subscription} from 'rxjs';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements ViewWillEnter, ViewWillLeave {
+    private reader$: Subscription;
 
-  constructor() {}
+    public constructor(private nfc: NFC, private ndef: Ndef) {
+    }
 
+    public async activateReaderMode() {
+        let isNfcEnable = true;
+        try {
+            await this.nfc.enabled();
+        } catch (error) {
+            isNfcEnable = false;
+            console.log('NFC is not enable on this device : activate it if your want the app to work properly');
+            console.log('NFC setting result : ', await this.nfc.showSettings());
+        }
+        if(!isNfcEnable) {
+            //Todo modify to give the option to open the option tab and display message on page and not in console
+            return;
+        }
+        console.log('NFC is enabled, let\'s go !');
+
+        let flags = this.nfc.FLAG_READER_NFC_A | this.nfc.FLAG_READER_NFC_V | this.nfc.FLAG_READER_NFC_B | this.nfc.FLAG_READER_NFC_F;
+
+        this.reader$ = this.nfc.readerMode(flags).subscribe(
+                tag => console.log(tag),
+                err => console.log('Error reading tag', err)
+        );
+    }
+
+    public async ionViewWillEnter() {
+        console.log('View will enter');
+        await this.activateReaderMode();
+    }
+
+    public async ionViewWillLeave() {
+        console.log('View will leave');
+        this.reader$.unsubscribe();
+    }
 }
+
+/*
+        this.readerMode$ = this.nfc.readerMode(flags)
+            .pipe(
+            )
+            .subscribe(
+                tag => console.log(JSON.stringify(tag)),
+                err => console.log('Error reading tag', err)
+            );
+*/
