ae1b6e63d774d77333e58b091dbd7889cd47922a
Thomas Alexaline <thomas.alexaline@wiilog.fr>
2022-07-13 12:07:46 +0200

    WIIS-7724: finished header component + CSS fix in header
    Fix a small bug in the css of the header component. Added as much logic as possible for now in the header component.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {HeaderMode} from '@app/components/header/header-mode.enum';
+import {NavServices} from '@app/services/nav/nav.services';
 
 @Component({
     selector: 'app-header',
@@ -13,14 +14,19 @@ export class HeaderComponent implements OnInit {
 
     public headerModeEnum = HeaderMode;
 
-    constructor() {
+    constructor(private navService: NavServices) {
     }
 
     ngOnInit() {
     }
 
-    public hihihih() {
-        console.log(5);
+    public backButtonClicked() {
+        //Todo maybe something to do to ensure we go back to the right page
+        this.navService.pop();
+    }
+
+    public disconnectButtonClicked() {
+        //Todo open the password check modal and act depending on result
     }
 
 }
